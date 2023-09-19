dea7d04bc2a9ac146483b75491eb9542bce53f95
ta mai anh <maianhtt0405@gmail.com>
2023-09-19 09:02:42 +0700

    tai code lan 1

---
+++ giai hpt.py
@@ -0,0 +1,9 @@
+import numpy as np
+A = np.array([(1,2),(3,4)])
+B = np.array([5,6])
+A1  = np.linalg.inv(A) # tạo ma trận nghich đảo
+print(A)
+print(B)
+print(A1)
+X = np.dot(A1,B)
+print('Nghiem cua he:',X)
